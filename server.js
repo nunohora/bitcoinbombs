06a1ca02c5e6818acb8e3162114a9d231c63b286
Nuno Hora <nunohora@Nunos-MacBook-Air.local>
2014-04-07 22:18:07 +0100

    added a simple websocket response

--- server.js
+++ server.js
@@ -51,7 +51,10 @@ server.listen(port);
 var io = io.listen(server);
 io.sockets.on('connection', function(socket){
   console.log('Client Connected');
-
+  socket.on('message', function(data){
+    socket.broadcast.emit('server_message',data);
+    socket.emit('server_message',data);
+  });
   socket.on('disconnect', function(){
     console.log('Client Disconnected.');
   });
