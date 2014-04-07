c0c37901854686bf4014626e25f9989eceae31aa
Nuno Hora <nunohora@Nunos-MacBook-Air.local>
2014-04-07 22:08:53 +0100

    second commit

---
+++ db.js
@@ -0,0 +1,13 @@
+//Connect to Database
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/bitcoinbombs');
+
+module.exports = {
+    initialize: function () {
+
+    },
+
+    createSchemas: function () {
+        
+    }
+}
